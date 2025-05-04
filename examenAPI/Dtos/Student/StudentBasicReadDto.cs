296cfdc91f0974ed1702b9d9a4f872c0eb80d04b
Moshe9647 <isaacmhp2001@gmail.com>
2025-05-04 16:25:54 -0600

    Added class 'StudentBasicDto' for 'GetCoursesWithStudents' method

---
+++ examenAPI/Dtos/Student/StudentBasicReadDto.cs
@@ -0,0 +1,8 @@
+namespace examenAPI.Dtos.Student
+{
+    public class StudentBasicReadDto
+    {
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+    }
+}
